b6973bdf3a78ee701490052ca7c3343e42c3bcad
c0d35 <coder@computer.org>
2014-04-28 13:24:47 +0200

    brrb

--- le_space.h
+++ le_space.h
@@ -25,22 +25,23 @@ enum class AccessScheme: bool
 };
 
 
-template< int _Dim, class _Topo, class _ElementT >
+template< int _Dim, typename _Topo, typename _ElementT >
 class TopologicalSpace {};
-template< int _Dim, class _Topo, class _ElementT >
+template< int _Dim, typename _Topo, typename _ElementT >
 class UniformSpace: public TopologicalSpace< _Dim, _Topo, _ElementT > {};
-template< int _Dim, class _Topo, class _Metric >
+template< int _Dim, typename _Topo, typename _Metric >
 class MetricSpace: public UniformSpace < _Dim, _Topo, _Metric > {};
 
 //kinda abstract simplex(?), one example for an element
 
-template< int _Dim, LinkType _LType, AccessScheme _AScheme,  template< class U, class V > class _Containment, template< class U > class _Allocator > class AbstractSimplex {};
+template< int _Dim, LinkType _LType, AccessScheme _AScheme,  template< class U, class V > class _Containment, template< class U > class _Allocator > struct AbstractSimplex {};
 
 template< int _Dim,  template< class U, class V > class _Containment, template< class U > class _Allocator >
-class AbstractSimplex< _Dim, LinkType::Single, AccessScheme::IndexAccessScheme, _Containment, _Allocator >
+struct AbstractSimplex< _Dim, LinkType::Single, AccessScheme::IndexAccessScheme, _Containment, _Allocator >
 {
     public:
         ptrdiff_t upper, opponent, next;
+        ptrdiff_t lower[_Dim + 1]; // why Dim + 1 you may ask? 'cause \binom{d+1}{d} = d + 1, u kno?
 };
 //
 
