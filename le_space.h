762afa69150dfca89ef464e6054ff276daf53af4
c0d35 <coder@computer.org>
2014-04-28 13:13:49 +0200

    add an abstract simplex ;3 and so on

--- le_space.h
+++ le_space.h
@@ -30,10 +30,14 @@ template< int _Dim, class _Topo, class _Metric >
 class MetricSPace: public UniformSpace < _Dim, _Topo, _Metric > {};
 
 //kinda abstract simplex(?), one example for an element
+template< int _Dim, LinkType _LType, AccessScheme _AScheme,  template< class U, class V > class _Containment, template< class U > class _Allocator > class AbstractSimplex {};
 
 template< int _Dim,  template< class U, class V > class _Containment, template< class U > class _Allocator >
 class AbstractSimplex< _Dim, LinkType::Single, AccessScheme::IndexAccessScheme, _Containment, _Allocator >
-{};
+{
+    public:
+        ptrdiff_t upper, opponent, next;
+};
 //
 
 #endif
