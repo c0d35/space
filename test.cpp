0afbc56a518b4aa9a58961e2be3d133665ef6aba
Administrator <administrator@notebook00.naske-rechtsanwaelte.eu>
2014-05-04 18:26:18 +0200

    nother test

--- test.cpp
+++ test.cpp
@@ -4,7 +4,9 @@
 
 int main()
 {
-    AbstractSimplex< 3, LinkType::Single, AccessScheme::Index, std::vector, std::allocator > tetra;
-    std::cout << "atetra::d = " << decltype(tetra)::d << std::endl;
-    return 0;
+	AbstractSimplex< 3, LinkType::Single, AccessScheme::Index, std::vector, std::allocator, Set< 3 >  > atetra;
+	AbstractSimplex< 6, LinkType::Single, AccessScheme::Index, std::vector, std::allocator, TopologicalSpace< 6 > > simplex6;
+	std::cout << "atetra::d = " << decltype(atetra)::d << std::endl;
+	std::cout << "simplex6::d = " << decltype(simplex6)::d << std::endl;
+	return 0;
 }
