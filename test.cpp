9474639256db3503814ae35a4d115200b506765a
c0d35 <coder@computer.org>
2014-05-09 18:28:00 +0200

    added euklidian space

--- test.cpp
+++ test.cpp
@@ -4,9 +4,13 @@
 
 int main()
 {
+
+
+    SimpleEuklidianSpace< 6 >::Vector< double > v;
 	AbstractSimplex< 3, LinkType::Single, AccessScheme::Index, std::vector, std::allocator, Set< 3 >  > atetra;
 	AbstractSimplex< 6, LinkType::Single, AccessScheme::Index, std::vector, std::allocator, TopologicalSpace< 6 > > simplex6;
 	std::cout << "atetra::d = " << decltype(atetra)::d << std::endl;
 	std::cout << "simplex6::d = " << decltype(simplex6)::d << std::endl;
+    std::cout << " SimpleEuklidianMetricTrait< 6, float >::Vector::d  = " << decltype(v)::d << std::endl;
 	return 0;
 }
